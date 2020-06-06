d2f8db5d29d872f80ba877db200d563fa2cece6e
YuliaRubanova <yul.rubanova@gmail.com>
2020-06-06 16:28:19 -0400

    Updated Edge predictor model to take a mask of added edges and compute proper attention

--- nets/edge_predictor.py
+++ nets/edge_predictor.py
@@ -17,6 +17,8 @@ class EdgeAttentionModel(AttentionModel):
     def __init__(self, **kwargs):
         super(EdgeAttentionModel, self).__init__(**kwargs)
         self.project_from_heads = nn.Linear(self.n_heads, 1, bias=False)
+        self.project_attention = nn.Linear(self.embedding_dim * 4, 1, bias=False)
+        self.project_compatibility = nn.Linear(self.embedding_dim // self.n_heads * 3 + 1, 1, bias=False)
 
     def forward(self, input, return_pi=False):
         """
@@ -78,20 +80,21 @@ class EdgeAttentionModel(AttentionModel):
 
             i += 1
 
-        # Return 1) Probabiliyu distributions 2) Chosen actions (edges) 
+        # Return 1) Probability distributions 2) Chosen actions (edges) 
         # 3) A combined adjacency matrix
         return torch.stack(probs, 1), torch.stack(actions, 1), torch.sum(torch.stack(action_masks, 1), 1)
 
 
     def _get_log_p(self, fixed, state, normalize=True):
         # Compute keys and values for the nodes
-        glimpse_K, glimpse_V, _ = self._get_attention_node_data(fixed, state)
+        glimpse_Q, glimpse_K, glimpse_V = self._get_attention_node_data(fixed, state)
 
         # Compute the mask
         mask = state.get_mask()
 
         # Compute logits (unnormalized log_p)
-        log_p = self._one_to_edge_logits(glimpse_K, glimpse_V, mask)
+        log_p = self._one_to_edge_logits(glimpse_Q, glimpse_K, glimpse_V,
+            mask, fixed.context_node_projected)
 
         assert not torch.isnan(log_p).any()
 
@@ -102,23 +105,21 @@ class EdgeAttentionModel(AttentionModel):
         return log_p, mask
 
 
-    def _compute_attention(self, glimpse_Q, glimpse_K, mask):
-        n_heads, batch_size, num_steps, embed_dim, _ = glimpse_Q.size()
-        key_size = val_size = embed_dim // self.n_heads
+    def _one_to_edge_logits(self, glimpse_Q, glimpse_K, glimpse_V, mask, graph_embedding):
+        # TODO: use different projections for two types of attention 
+        # todo: do we want to use a mask in _compute_parwise_attention x?
+        node_attention = self._compute_parwise_attention(glimpse_Q, glimpse_K) 
 
-        # Batch matrix multiplication to compute compatibilities (n_heads, batch_size, num_steps, graph_size)
-        attention = torch.matmul(glimpse_Q, glimpse_K.transpose(-2, -1))
-
-        attention = self.project_from_heads(attention.permute(1, 2, 3, 4, 0)).squeeze(-1)
-
-        if mask is not None:
-            attention[mask.expand_as(attention)] = -math.inf
-
-        return attention
+        node_connectivity_glimpse = self._compute_graph_glimpse(
+            glimpse_V, glimpse_Q, glimpse_K, mask, graph_embedding)
+        node_connectivity_glimpse = node_connectivity_glimpse.unsqueeze(0)
 
 
-    def _one_to_edge_logits(self, query, glimpse_K, mask):
-        edge_attention = self._compute_attention(query, glimpse_K, mask) 
+        connectivity_attention = self._compute_parwise_attention(
+            node_connectivity_glimpse, node_connectivity_glimpse) 
+        
+        edge_attention = self._compute_final_attention_mask(
+            [node_attention, connectivity_attention], mask)
 
         assert not torch.isnan(edge_attention).any()
 
@@ -131,3 +132,77 @@ class EdgeAttentionModel(AttentionModel):
         assert not torch.isnan(edge_attention).any()
 
         return edge_attention
+
+
+    def _compute_final_attention_mask(self, attention, mask):
+        if isinstance(attention, list):
+            attention = torch.cat(attention, -1)
+
+        attention = self.project_attention(attention).squeeze(-1)
+
+        if mask is not None:
+            attention[mask.expand_as(attention)] = -math.inf
+
+        return attention
+
+
+
+
+    def _compute_parwise_attention(self, glimpse_Q, glimpse_K):
+        n_heads, batch_size, num_steps, n_nodes, embed_dim = glimpse_Q.size()
+        key_size = val_size = embed_dim // self.n_heads
+
+        # Dot product attention
+        # attention = torch.matmul(glimpse_Q, glimpse_K.transpose(-2, -1))
+        # attention = self.project_from_heads(attention.permute(1, 2, 3, 4, 0)).squeeze(-1)
+
+        expanded_shape = (n_heads, batch_size, num_steps, n_nodes, n_nodes, embed_dim)
+
+        attention = torch.cat((
+            glimpse_Q.unsqueeze(3).expand(expanded_shape),
+            glimpse_K.unsqueeze(4).expand(expanded_shape)
+            ), 
+        -1)
+
+        attention = attention.permute(1, 2, 3, 4, 5, 0)
+
+        attention = attention.reshape(batch_size, num_steps, 
+            n_nodes, n_nodes, -1)
+
+        return attention
+
+ 
+    def _compute_graph_glimpse(self, query, glimpse_K, glimpse_V, mask, graph_embedding):
+        # Compute glimpse only on the graph that is not included into the path yet
+        batch_size, num_steps, n_nodes, embed_dim = query.size()
+        key_size = val_size = embed_dim // self.n_heads
+
+        # Compute the glimpse, rearrange dimensions so the dimensions are (n_heads, batch_size, num_steps, 1, key_size)
+        glimpse_Q = query.view(batch_size, num_steps, n_nodes, self.n_heads, key_size).permute(3, 0, 1, 2, 4)
+        graph_embedding = graph_embedding.view(batch_size, num_steps, self.n_heads, key_size).permute(2, 0, 1, 3)
+
+        # Batch matrix multiplication to compute compatibilities (n_heads, batch_size, num_steps, graph_size)
+        #compatibility = torch.matmul(glimpse_Q, glimpse_K.transpose(-2, -1)) / math.sqrt(glimpse_Q.size(-1))
+
+
+        expanded_shape = (self.n_heads, batch_size, num_steps, n_nodes, n_nodes, key_size)
+
+        compatibility = torch.cat((
+            glimpse_Q.unsqueeze(3).expand(expanded_shape),
+            glimpse_K.unsqueeze(4).expand(expanded_shape),
+            mask[None,:,:,:,:,None].expand((*expanded_shape[:-1], 1)).float(),
+            graph_embedding[:,:,:,None,None,:].expand(expanded_shape),
+            ), 
+        -1)
+
+        compatibility = self.project_compatibility(compatibility).squeeze(-1)
+
+        # Batch matrix multiplication to compute heads (n_heads, batch_size, num_steps, val_size)
+        heads = torch.matmul(torch.softmax(compatibility, dim=-1), glimpse_V)
+
+        # Project to get glimpse/updated context node embedding (batch_size, num_steps, embedding_dim)
+        glimpse = self.project_out(
+            heads.permute(1, 2, 3, 0, 4).contiguous().view(batch_size, num_steps, n_nodes, self.n_heads * val_size))
+
+        return glimpse.squeeze(-2)
+
